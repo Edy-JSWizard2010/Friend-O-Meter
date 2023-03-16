970a7bdb2a757c9d45593d53cd4fe3b774a6d0b5
Edy-JSWizard2010 <128014199+Edy-JSWizard2010@users.noreply.github.com>
2023-03-16 09:29:53 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,182 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is my favorite color?",
+    answer: "Red",
+    options: [
+      "Orange",
+      "Puple",
+      "Black",
+      "Red"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is my favorite food?",
+    answer: "Shawarma[every single one]",
+    options: [
+      "Spicy Ramen",
+      "Shawarma[every single one]",
+      "Chicken Fiesta(Pizza)",
+      "Japanese Beef Wagyu"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who is my favorite friend?(Boys)",
+    answer: "Danny",
+    options: [
+      "Danny",
+      "Lalith Aditya",
+      "Harish",
+      "Hemanthan"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What instrument am i learning to play?",
+    answer: "Guitar",
+    options: [
+      "Guitar",
+      "Flute",
+      "Piano",
+      "Ukelele"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is my favorite sports?",
+    answer: "Football",
+    options: [
+      "Esports",
+      "Baseball",
+      "Football",
+      "Cricket"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+   {
+     numb: 6,
+     question: "What is are brothers name?",
+     answer: "Both Elwin Zacharia & Monish",
+     options: [
+      "Monish",
+      "Gold Fish",
+      "Elwin Zacharia",
+      "Both Elwin Zacharia & Monish"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What is my favorite subject?",
+    answer: "Both Math and SST",
+    options: [
+     "Science",
+     "Maths",
+     "SST",
+     "Both Math and SST"
+   ]
+ },
+
+ {
+  numb: 8,
+  question: "What is my favorite teacher?",
+  answer: "Mubarak Mam",
+  options: [
+   "Mubarak Mam",
+   "Mukesh Sir",
+   "Kanchana Mam",
+   "Jagan Sir"
+ ]
+},
+
+{
+  numb: 9,
+  question: "What is my D.O.B?",
+  answer: "day-21 month-7 year-2010",
+  options: [
+   "day-52 month-2 year-1",
+   "day-31 month-12 year-2050",
+   "day-21 month-10 year-2010",
+   "day-21 month-7 year-2010"
+ ]
+},
+
+{
+  numb: 10,
+  question: "What is my hobby?",
+  answer: "Coding & Developing apps/games",
+  options: [
+   "Drwaing",
+   "Coding & Developing apps/games",
+   "Singing",
+   "Sitting simply"
+ ]
+},
+
+{
+  numb: 11,
+  question: "What is my favorite programming language?",
+  answer: "JS",
+  options: [
+   "Java",
+   "Python",
+   "JS",
+   "R"
+ ]
+},
+
+{
+  numb: 12,
+  question: "Which is my favorite festival?",
+  answer: "Christmas",
+  options: [
+   "Easter",
+   "Pongal",
+   "Christmas",
+   "Independence day"
+ ]
+},
+
+{
+  numb: 13,
+  question: "What is my phone model?",
+  answer: "Realme 5Pro",
+  options: [
+   "Oppo F3",
+   "Moto x4",
+   "Realme 5Pro",
+   "Samsung zFold"
+ ]
+},
+
+{
+  numb: 14,
+  question: "What is my favorite car brand?",
+  answer: "Honda",
+  options: [
+   "Porsche",
+   "BMW",
+   "Honda",
+   "Lamborghini"
+ ]
+},
+
+{
+  numb: 15,
+  question: "Who is the G.O.A.T[football]?",
+  answer: "Ronaldo",
+  options: [
+   "Messi",
+   "Neymar",
+   "Mbappé",
+   "Ronaldo"
+ ]
+},
+
+
+];
